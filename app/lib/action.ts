ca3712c488323c43284e7ae7c8f5f9fd625cddb0
ArmannGokdemir <armangokde@gmail.com>
2024-02-14 08:15:52 +0300

    added sign in out capabilities

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -5,6 +5,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { stat } from 'fs';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = zod.object({
   id: zod.string(),
@@ -97,7 +99,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
   try {
     await sql`
  DELETE FROM invoices WHERE id = ${id}`;
@@ -105,4 +107,23 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice' };
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
 }
